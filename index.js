7942021a5c83102e5488325ea3a3b6c0091cf915
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-04-15 00:28:30 +0300

    task 1 done

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 // Итерационный подсчет факториала
 function factIterat(num){
-
+    let factorial = 1;
+    if (num < 0 || num % 1 !== 0) {
+        return prompt = 'Введите целое неотрицательное число'
+    } else if (num == 0 || num == 1 ) {
+        return num;
+    } else {
+        for(let i=1; i<= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
+console.log(factIterat(4));
 
 // Рекурсивный расчет факториала
 function factRec(num){
- 
+    if (num < 0 || num % 1 !== 0) {
+        return prompt = 'Введите целое неотрицательное число'
+    } else if (num == 0 || num == 1 ) {
+        return num;
+    } else {
+        return num *= factRec(num-1);
+        }
 }
+console.log(factRec(5));
